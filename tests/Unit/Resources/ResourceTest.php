9e446e8b8197d544b629f070048839023e485a4c
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-05-09 22:49:30 +0400

    WIP. Devices endpoint added

--- tests/Unit/Resources/ResourceTest.php
+++ tests/Unit/Resources/ResourceTest.php
@@ -7,7 +7,7 @@ use Orkhanahmadov\Sipgate\Resources\Resource;
 use Orkhanahmadov\Sipgate\Tests\TestCase;
 use Orkhanahmadov\Sipgate\Resources\User;
 
-class UserTest extends TestCase
+class ResourceTest extends TestCase
 {
     public function test_throws_exception_if_property_is_not_set()
     {
